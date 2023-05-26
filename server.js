6ba06fbb36a89d12a6c72fb6b278cccad1be8f2e
programero <alamirhussen05@gmail.com>
2023-05-26 23:21:02 +0300

    port

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ io.on('connection', socket => {
   })
 })
 
-const PORT =  3000;
+const PORT = process.env.PORT|| 3000;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
